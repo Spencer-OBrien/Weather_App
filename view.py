ad2ace94263081404a043dfc1f65a076572ede2d
Spencer-OBrien <spencerobrien@live.com>
2024-08-06 18:36:31 +0200

    add .env, add flag and add more weather info

--- view.py
+++ view.py
@@ -3,10 +3,11 @@ from weather import city_weather
 
 views = Blueprint(__name__, "views")
 
-@views.route('/', methods=["GET", "POST"])
+
+@views.route("/", methods=["GET", "POST"])
 def home():
     weather_info = None
     if request.method == "POST":
         city_name = request.form["city"]
         weather_info = city_weather(city_name)
-    return render_template('index.html', weather_info=weather_info)
+    return render_template("index.html", weather_info=weather_info)
--- weather.py
+++ weather.py
@@ -1,16 +1,38 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
+from datetime import datetime
 
-API_KEY = r"21354f52bec7021538a451e50d85f404"
+load_dotenv(".env")
+
+API_KEY = os.getenv("API_KEY")
 KELVIN = 273.15
+
 
 def city_weather(city_name):
     city_name = city_name.capitalize()
-    
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}"
+
+    url = (f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}")
 
     response = requests.get(url)
     data = json.loads(response.text)
-    temp = int(data["main"]["temp"] - KELVIN)
 
-    return f"{city_name} {temp}°"
+    temp = int(data["main"]["temp"] - KELVIN)
+    sunrise = datetime.utcfromtimestamp(data["sys"]["sunrise"] + data["timezone"]).strftime("%H:%M")
+    sunset = datetime.utcfromtimestamp(data["sys"]["sunset"] + data["timezone"]).strftime("%H:%M")
+    current_time = datetime.utcfromtimestamp(data["dt"] + data["timezone"]).strftime("%H:%M")
+    description = data["weather"][0]["description"]
+    country = data["sys"]["country"]
+
+    weather_data = {
+        "city": city_name,
+        "temp": temp,
+        "sunrise": sunrise,
+        "sunset": sunset,
+        "current_time": current_time,
+        "description": description.capitalize(),
+        "country": country,
+    }
+
+    return weather_data
